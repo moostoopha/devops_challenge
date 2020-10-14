87f0136248a18974142918b520b4421a6ff45bf7
Mustafa Hasan <mustafahasan@Mustafa-hassan.local>
2020-10-14 19:19:20 +0500

    using Terraform or Cloudformation to provision one Linux VM in AWS capable to run docker containers and with the port 80 exposed to the internet.

--- ops/main.tf
+++ ops/main.tf
@@ -76,27 +76,9 @@ resource "aws_lb_listener" "http" {
   default_action {
     type = "redirect"
 
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
   }
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = "${aws_lb.nginx_alb.arn}"
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "${var.certificate_arn}"
-
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.asg.arn}"
-  }
-}
 
 resource "aws_lb_target_group" "asg" {
   name = "${var.r53_name}-asg"
